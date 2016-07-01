063cc3e6be098f911bb5a268510e1966dc242689
Phil <Phil>
2016-07-01 23:32:53 +0100

    Need to test with a function generator, added screen support, buffers work (likely). System fails if too much of the screen is written to. Check what the actual 200ms timer should be set to.

--- project2.c
+++ project2.c
@@ -28,28 +28,187 @@ volatile unsigned uTimerFlag;
 
 unsigned long ulAdcVal;
 unsigned uTimerCount = 0;
-unsigned long ulBuffer1[10], ulBuffer2[10];
+unsigned long ulBuffer1[100], ulBuffer2[100];
 unsigned uIndexCounter = 0;
 unsigned uBufferSelect;
 double dAvgADCVal = 0;
-char cAdcADCVal;
+char cAdcADCVal, cTimerCount, cBufferSelect, cVal, str[100], test[100];
 
 
+//*****************************************************************************
+//
+// ADC Interrupt Handler
+//
+// Triggers when an ADC event has finished reading. Clears the interrupt and
+// sets a flag.
+//
+//*****************************************************************************
 void ADC_IsrHandler()
 {
-	ADC_IntClear();
-	uAdcFlag = 1;
+	ADC_IntClear(); // clear interrupt
+	uAdcFlag = 1; // raise flag
 
 }
 
 
+//*****************************************************************************
+//
+// Timer Interrupt Handler
+//
+// Triggered by a timer after a set amount of time. Clears the interrupt,
+// starts an ADC sample, and raises a flag.
+//
+//*****************************************************************************
 void Timer_IsrHandler()
 {
-	Timer_IntClear();
-	uTimerFlag = 1;
+	Timer_IntClear(); // clear interrupt
+	ADC_StartSample(); // start adc sample
+	uTimerFlag = 1; // raise flag
+}
+
+/*
+void swap(char first, char second)
+{
+	char temporary;
+
+	temporary = first;
+	first = second;
+	second = first;
 }
 
 
+// reverses a string
+void reverse(char str[], int length)
+{
+    int start = 0;
+    int end = length -1;
+    while (start < end)
+    {
+        swap(*(str+start), *(str+end));
+        start++;
+        end--;
+    }
+}
+ 
+// Implementation of itoa()
+char* itoa(int num, char* str, int base)
+{
+    int i = 0;
+    //bool isNegative = false;
+    int isNegative = 0;
+ 
+    // Handle 0 explicitely, otherwise empty string is printed for 0
+    if (num == 0)
+    {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+ 
+    // In standard itoa(), negative numbers are handled only with 
+    // base 10. Otherwise numbers are considered unsigned.
+    if (num < 0 && base == 10)
+    {
+        //isNegative = true;
+        isNegative = 1;
+        num = -num;
+    }
+ 
+    // Process individual digits
+    while (num != 0)
+    {
+        int rem = num % base;
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+        num = num/base;
+    }
+ 
+    // If number is negative, append '-'
+    if (isNegative)
+        str[i++] = '-';
+ 
+    str[i] = '\0'; // Append string terminator
+ 
+    // Reverse the string
+    reverse(str, i);
+ 
+    return str;
+}*/
+
+
+//*****************************************************************************
+//
+// Average Buffer
+//
+// Takes a buffer of a certain length and computes the average of the buffer.
+//
+//*****************************************************************************
+double Average_Buffer(unsigned long* buffer)
+{
+	double average = 0;
+	double length = 100.0; // length of the buffer to be averaged
+
+	// sum the buffer into one variable 
+	for(int i=0;i<length;i++)
+	{
+		average += buffer[i];
+	}
+
+	// determine the average by dividing the summed buffer by its length
+	average /= length;
+
+	return average;
+}
+
+
+//*****************************************************************************
+//
+// OLED Display Power Level
+//
+// Takes in a value indicative of the power level on an adc pin and displays it
+// as a bar that changes height and brightness according to the power level
+//
+//*****************************************************************************
+void OLED_DisplayPowerLvl(double power)
+{
+	unsigned height, intensity, index;
+	double amplification = 20; // software amplifier
+	unsigned char array[500]; // array containing power bar information
+	unsigned Y_LIM = 40; // max height of the bar
+	unsigned X_LIM = 10; // width of the bar
+
+	//IntMasterDisable();
+
+	height = (int)(((power*amplification)/1023.0)*X_LIM);
+
+	intensity = (int)(height*0.1579);
+
+	intensity = ((intensity << 4) | intensity);
+
+	for(index=0;index < (X_LIM*Y_LIM/2);index++)
+	{
+		if(index < ((Y_LIM - height) * (X_LIM / 2.0) - 1))
+		{
+			array[index] = intensity;
+		}
+		else
+		{
+			array[index] = 0;
+		}
+	}
+
+	RIT128x96x4ImageDraw(array, 0, 0, X_LIM, Y_LIM);
+
+	//IntMasterEnable();
+}
+
+
+//*****************************************************************************
+//
+// Main
+//
+// Initializes all components and peripherals.
+//
+//*****************************************************************************
 int main(void)
 {
 	// Initialize ADC
@@ -72,73 +231,50 @@ int main(void)
 
 	while(1)
 	{
-		// If timer interrupt was triggered count up
+		// only occurs after timer event
 		if(uTimerFlag == 1)
 		{
-			uTimerFlag = 0;
+			// lower flag
+			uTimerFlag = 0; 
 
-			uTimerCount++;
+			// update counter
+			uTimerCount++; 
 
-			if(uTimerCount == 100)
+			// check if 200 ms have passed
+			if(uTimerCount == 99)
 			{
-				uTimerCount = 0;
-				dAvgADCVal = 0; // reset average
+				// reset counter
+				uTimerCount = 0; 
 
-				if(uBufferSelect == 1)
-				{
-					//RIT128x96x4Clear();
-					RIT128x96x4StringDraw("Hello", 36,  0, 15);
-					// switch buffers early so the adc doesnt overwrite anything or mess up
-					uBufferSelect = 0;
+				// determine the average voltage level from the correct buffer
+				dAvgADCVal = (uBufferSelect == 1) ? Average_Buffer(ulBuffer1) : Average_Buffer(ulBuffer2);
+				
+				// switch buffers
+				uBufferSelect ^= 1; 
+				
+				//itoa(dAvgADCVal*20.0, test, 10);
 
-					// average second buffer
-					for(uIndexCounter=0; uIndexCounter<uTimerCount; uIndexCounter++)
-					{
-						dAvgADCVal += ulBuffer2[uIndexCounter]; // grab index and roll into average
-						ulBuffer1[uIndexCounter] = 0; // reset array index
-					}
-				}
-				else
-				{
-					//RIT128x96x4Clear();
-					RIT128x96x4StringDraw("Bye", 36,  16, 15);
-					// switch buffers
-					uBufferSelect = 1;
-
-					// average first buffer
-					for(uIndexCounter=0; uIndexCounter<uTimerCount; uIndexCounter++)
-					{
-						dAvgADCVal += ulBuffer1[uIndexCounter]; // grab index and roll into average
-						ulBuffer1[uIndexCounter] = 0; // reset array index
-					}
-				}
-				// average and display
-				dAvgADCVal /= 10;
-				cAdcADCVal = (char)dAvgADCVal;
-				//RIT128x96x4StringDraw(&cAdcADCVal, 2, 2, 15);
-				//RIT128x96x4StringDraw("Hello", 36,  0, 15);
+				// Display the power level by adjusting a bar
+				OLED_DisplayPowerLvl(dAvgADCVal); 
 			}
-
-			//RIT128x96x4StringDraw("UART Echo", 36,  0, 15);
-
-			ADC_StartSample();
 		}
 
+		// only occurs after adc event
 		if(uAdcFlag == 1)
 		{
+			// lower flag
 			uAdcFlag = 0;
 
-			//RIT128x96x4Clear();
+			// get an adc read
+			ADC_GetSample(&ulAdcVal); 
 
-			//fill buffer here
+			//itoa(ulAdcVal, str, 10);
+
+			//check which buffer is active and fill the appropriate one
 			if(uBufferSelect == 1)
-			{
-				ADC_GetSample(&ulBuffer1[uTimerCount-1]);
-			}
+				ulBuffer1[uTimerCount] = ulAdcVal;
 			else
-			{
-				ADC_GetSample(&ulBuffer2[uTimerCount-1]);
-			}
+				ulBuffer2[uTimerCount] = ulAdcVal;
 		}
 	}
 }
